327a5e1c68d9c4bd75ca2c0d62d94f95ce1fc19a
Michał <m12@onet.com.pl>
2023-10-04 19:37:26 +0200

    Add project files.

---
+++ Mole.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security;
+using System.Security.Cryptography;
+
+namespace DuplicateFileFinderCs {
+    public class Mole {
+
+        /// <summary>
+        /// Return a Dictionary<string, string> of all duplicate files found in "path" 
+        /// </summary>
+        /// <param name="path"></param>
+        public Dictionary<string, string> DigIn(string path) {
+            Console.WriteLine("The Nibdz is digging...");
+
+            string[] files = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+            Dictionary<string, string> FileHashPair = new();
+            Dictionary<string, string> duplicateHashes = new();
+            foreach (string filePath in files) {
+                using (MD5 md5 = MD5.Create())
+                using (FileStream stream = File.OpenRead(filePath)) {
+                    byte[] hashData = md5.ComputeHash(stream);
+                    string hash = BitConverter.ToString(hashData).Replace("-", "").ToLowerInvariant();
+                    FileHashPair.Add(filePath, hash);
+                }
+            }
+
+            duplicateHashes = FileHashPair
+                .GroupBy(kv => kv.Value)
+                .Where(group => group.Count() > 1)
+                .SelectMany(group => group)
+                .ToDictionary(kv => kv.Key, kv => kv.Value);
+
+
+            return duplicateHashes;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using DuplicateFileFinderCs;
+
+string searchDir = ".";
+
+if (args.Length > 0) {
+    searchDir = args[0];
+    if (!Directory.Exists(searchDir)) {
+        Console.WriteLine($"{searchDir} is not a valid path");
+        Environment.Exit(1);
+    }
+}
+
+Mole mole = new();
+var duplicates = mole.DigIn(searchDir);
+
+if (duplicates.Count == 0) {
+    Console.WriteLine($"The mole was not able to find any duplicate files in {searchDir}");
+    return;
+}
+
+foreach (var dup in duplicates) {
+    Console.WriteLine(dup.Key);
+}
+
